0955093f1634574164a6a5565e5ad7c641744313
wenhaolu <wenhaolu.kevin@gmail.com>
2024-05-15 13:53:41 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Signup from './components/Signup';
 const MOVIE_API = process.env.REACT_APP_MOVIEAPI;
 const GOOGLE_API = process.env.REACT_APP_GOOGLEAPI;
 
-console.log(MOVIE_API);
+console.log(process.env.REACT_APP_MOVIEAPI);
 
 function App() {
     const { user } = useAuth();
